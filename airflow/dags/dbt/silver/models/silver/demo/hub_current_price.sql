85ffa29187f5cbf7dd694e25cc9448f20bf9c23f
Edward Nguyen <deanblog.info@gmail.com>
2024-09-16 21:52:58 +1000

    fix incremental dbt45

--- airflow/dags/dbt/silver/models/silver/demo/hub_current_price.sql
+++ airflow/dags/dbt/silver/models/silver/demo/hub_current_price.sql
@@ -2,11 +2,12 @@ WITH source_data AS (
     SELECT
         hk_current_price,  -- hash key for current_price
         id as business_key_price
+        CURRENT_TIMESTAMP() AS hub_load_datetime
     FROM {{ source('STAGING_TABLE', 'STG_HASH_CURRENT_PRICE_SYMBOL') }}  -- reflect to stg_hash_current_price_symbol
 )
 
 SELECT
     hk_current_price,   -- hash key for current_price
-    business_key_price,       -- business key for price
-    CURRENT_TIMESTAMP() AS hub_load_datetime
+    business_key_price,  
+    hub_load_datetime     -- business key for price
 FROM source_data
