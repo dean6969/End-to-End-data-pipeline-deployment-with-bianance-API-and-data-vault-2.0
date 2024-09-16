4afe7c8094a01f805f8db7a782da693efeeb3ae8
Edward Nguyen <deanblog.info@gmail.com>
2024-09-16 22:08:18 +1000

    fix incremental bugs43

--- airflow/dags/dbt/silver/models/silver/demo/sat_current_price.sql
+++ airflow/dags/dbt/silver/models/silver/demo/sat_current_price.sql
@@ -1,6 +1,6 @@
 WITH source_data AS (
     SELECT
-        hk_current_price, 
+        *, 
         CURRENT_TIMESTAMP() AS sat_load_datetime
     FROM {{ source('STAGING_TABLE', 'STG_HASH_CURRENT_PRICE_SYMBOL') }}  
 )
